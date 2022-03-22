2d05425d562a958505b60bbde2714fa194aeba5d
Wolf <wolf2wulf@gmail.com>
2022-03-22 16:42:04 +0100

    new download script

--- data/download.sh
+++ data/download.sh
@@ -1,72 +1,10 @@
 #!/bin/bash
 
-# Make directory for raws
-if [ ! -d data/raw ]; then
-    mkdir data/raw
-fi
-
 # Download subject data
-if ! test -f data/raw/ds000117_R1.0.0_sub01-04.zip; then
-    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_sub01-04.zip"
-    unzip data/raw/ds000117_R1.0.0_sub01-04.zip
-    mv data/raw/ds000117_R1.0.0/* data/raw
-    rm -rf data/raw/ds000117_R1.0.0
-fi
-if ! test -f data/raw/ds000117_R1.0.0_derivatives_sub01-04.zip; then
-    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_derivatives_sub01-04.zip"
-    unzip data/raw/ds000117_R1.0.0_derivatives_sub01-04.zip
-    mv data/raw/ds000117_R1.0.0/* data/raw
-    rm -rf data/raw/ds000117_R1.0.0
-fi
-#if ! test -f data/raw/ds000117_R1.0.0_sub05-08.zip; then
-#    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_sub05-08.zip"
-#    unzip data/raw/ds000117_R1.0.0_sub05-08.zip
-#    mv data/raw/ds000117_R1.0.0/* data/raw
-#    rm -rf data/raw/ds000117_R1.0.0
-#fi
-#if ! test -f data/raw/ds000117_R1.0.0_derivatives_sub05-08.zip; then
-#    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_derivatives_sub05-08.zip"
-#    unzip data/raw/ds000117_R1.0.0_derivatives_sub05-08.zip
-#    mv data/raw/ds000117_R1.0.0/* data/raw
-#    rm -rf data/raw/ds000117_R1.0.0
-#fi
-#if ! test -f data/raw/ds000117_R1.0.0_sub09-12.zip; then
-#    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_sub09-12.zip"
-#    unzip data/raw/ds000117_R1.0.0_sub09-12.zip
-#    mv data/raw/ds000117_R1.0.0/* data/raw
-#    rm -rf data/raw/ds000117_R1.0.0
-#fi
-#if ! test -f data/raw/ds000117_R1.0.0_derivatives_sub09-12.zip; then
-#    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_derivatives_sub09-12.zip"
-#    unzip data/raw/ds000117_R1.0.0_derivatives_sub09-12.zip
-#    mv data/raw/ds000117_R1.0.0/* data/raw
-#    rm -rf data/raw/ds000117_R1.0.0
-#fi
-#if ! test -f data/raw/ds000117_R1.0.0_sub13-16.zip; then
-#    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_sub13-16.zip"
-#    unzip data/raw/ds000117_R1.0.0_sub13-16.zip
-#    mv data/raw/ds000117_R1.0.0/* data/raw
-#    rm -rf data/raw/ds000117_R1.0.0
-#fi
-#if ! test -f data/raw/ds000117_R1.0.0_derivatives_sub13-16.zip; then
-#    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_derivatives_sub13-16.zip"
-#    unzip data/raw/ds000117_R1.0.0_derivatives_sub13-16.zip
-#    mv data/raw/ds000117_R1.0.0/* data/raw
-#    rm -rf data/raw/ds000117_R1.0.0
-#fi
-
-# Download metadata
-if ! test -f data/raw/ds000117_R1.0.0_metadata.zip; then
-    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_metadata.zip"
-    unzip data/raw/ds000117_R1.0.0_metadata.zip   
-    mv data/raw/ds000117_R1.0.0/* data/raw
-    rm -rf data/raw/ds000117_R1.0.0
-fi
-
-# Download stimuli
-if ! test -f data/raw/ds000117_R1.0.0_stimuli.zip; then
-    wget -P data/raw "https://s3.amazonaws.com/openneuro/ds000117/ds000117_R1.0.0/compressed/ds000117_R1.0.0_stimuli.zip"
-    unzip data/raw/ds000117_R1.0.0_stimuli.zip
-    mv data/raw/ds000117_R1.0.0/* data/raw
-    rm -rf data/raw/ds000117_R1.0.0
+if ! test -f data/raw/sub1; then
+    mkdir data/raw/sub1
+    wget -P data/raw "http://openfmri.s3.amazonaws.com/tarballs/ds117_R0.1.1_sub001_raw.tgz"
+    tar xvzf data/raw/ds117_R0.1.1_sub001_raw.tgz data/raw
+    mv data/raw/ds117_R0.1.1_sub001_raw/MEG/* data/raw/sub1
+    rm -rf data/raw/ds117_R0.1.1_sub001_raw data/raw/*.tgz
 fi
