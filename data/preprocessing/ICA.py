a761fd69317f14d1f88b3ed3308f45f74bcbbdfd
Wolf <wolf2wulf@gmail.com>
2022-03-25 17:26:16 +0100

    Added gitignore + added preproc code + start ICA code

--- data/preprocessing/ICA.py
+++ data/preprocessing/ICA.py
@@ -2,31 +2,17 @@ import mne
 import os
 
 # Create ICA-processed folder
-os.mkdir("/data/brussel/102/vsc10248/data")
-os.mkdir("/data/brussel/102/vsc10248/data/processed")
+os.mkdir("data/ICA_processed")
 
 # Loop over subjects
-for subject_folder in os.listdir("/scratch/brussel/102/vsc10248/data/raw"):
+for subject_folder in os.listdir("data/processed"):
 
-    # Make folder for subject processed
-    os.mkdir("/data/brussel/102/vsc10248/data/processed/" + subject_folder)
+    # Make folder for subject ICA-processed
+    os.mkdir("data/ICA_processed/" + subject_folder)
 
     # Loop over runs
-    for run_id, run_file in enumerate(os.listdir("/scratch/brussel/102/vsc10248/data/raw/" + subject_folder)):
+    for run_id, run_file in enumerate(os.listdir("data/processed/" + subject_folder)):
 
         # Read raw
-        raw = mne.io.read_raw_fif("/scratch/brussel/102/vsc10248/data/raw/" + subject_folder + "/" + run_file)
-
-        # Pick MEG and EEG
-        raw.pick_types(meg='mag', eeg=True, stim=True)
-        print(raw)
-
-        # Resample according to Shannon-Nyquist and highest freq band
-        raw.resample(250)
-
-        # Bandpass filter between lowest and highest of freq bands
-        raw.filter(l_freq=0, h_freq=100)
-        raw.notch_filter(50)
-
-        # Save
-        raw.save("/data/brussel/102/vsc10248/data/processed/" + subject_folder + "/" + str(run_id) + ".fif")
+        raw = mne.io.read_raw_fif("data/processed/" + subject_folder + "/" + run_file)
+        print(raw.info)
