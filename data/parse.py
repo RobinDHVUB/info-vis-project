53d3076e1922f715bd4444f2b574875e4e0001ba
Wolf <wolf2wulf@gmail.com>
2022-05-03 17:47:35 +0200

    typo correction + windowing with event selection

--- data/parse.py
+++ data/parse.py
@@ -14,7 +14,7 @@ event_names = {
     13: "Initial Unfamiliar Face",
     14: "Immediate Repeat Unfamiliar Face",
     15: "Delayed Repeat Unfamiliar Face",
-    17: "Initial Scambled Face",
+    17: "Initial Scrambled Face",
     18: "Immediate Repeat Scrambled Face",
     19: "Delayed Repeat Scrambled Face",
 }
@@ -51,20 +51,27 @@ def parse_run(subject, run, eeg_channels, meg_channels):
         "data/processed/subject" + str(subject) + "/run" + str(run) + "/processed.fif"
     )
 
-    # Extract events
-    events = mne.find_events(raw, stim_channel=["STI101"])
-    correct_events = []
-    for event in events:
-        if event[1] in event_names.keys():
-            correct_events.append([event[0], event[1]])
-        elif event[2] in event_names.keys():
-            correct_events.append([event[0], event[2]])
-
     return (
         raw.get_data(picks=eeg_channels, units="uV") if len(eeg_channels) > 0 else [],
         raw.get_data(picks=meg_channels, units="fT") if len(meg_channels) > 0 else [],
-        correct_events
+        extract_events(raw)
     )
+
+def extract_events(raw, event_ids=None):
+    
+    # Get events from raw
+    events = mne.find_events(raw, stim_channel=["STI101"])
+    
+    # Select 
+    selected_events = []
+    selection = event_names.keys() if event_ids is None else event_ids
+    for event in events:
+        if event[1] in selection:
+            selected_events.append([event[0], event[1]])
+        elif event[2] in selection:
+            selected_events.append([event[0], event[2]])
+
+    return selected_events
 
 
 # -----
@@ -72,17 +79,18 @@ def parse_run(subject, run, eeg_channels, meg_channels):
 # -----
 
 
-def parse_windows(subject, run, eeg_channels, meg_channels, tmin, tmax):
+def parse_windows(subject, run, eeg_channels, meg_channels, event_ids, tmin, tmax):
     """
     Parses and returns windows for a given subject's run
     ---
     input:
         subject number [1-19]
         run number [1-6]
-        list of eeg channels (those returned by parse_eeg_names)
-        list of meg channels (those return by parse_meg_names)
-        time to cut before the event
-        time to cut after the event
+        list of eeg channels
+        list of meg channels
+        list of event ids to average over
+        time to cut before the event (must be >= minimum_min)
+        time to cut after the event  (must be <= maximum_max)
     ---
     output:
         EEG windows AVG in microvolt (n_eeg_channels, n_samples)
@@ -93,9 +101,10 @@ def parse_windows(subject, run, eeg_channels, meg_channels, tmin, tmax):
     raw = mne.io.read_raw_fif(
         "data/processed/subject" + str(subject) + "/run" + str(run) + "/processed.fif"
     )
+
     windows = mne.Epochs(
         raw,
-        mne.find_events(raw, stim_channel=["STI101"]),
+        numpy.insert(extract_events(raw, event_ids), 1, 0, axis=1),
         picks=eeg_channels + meg_channels,
         tmin=tmin,
         tmax=tmax,
