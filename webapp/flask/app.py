0d04f19726ae11bfffdd3025f6f79c5df442ab80
RobinDHVUB <72613839+RobinDHVUB@users.noreply.github.com>
2022-05-04 18:32:30 +0200

    added 1-page bokeh

--- webapp/flask/app.py
+++ webapp/flask/app.py
@@ -26,71 +26,23 @@ def index():
     )
 
 
-patients = [
-    {"id": 0, "name": "patient 0", "age": 23, "gender": "man"},
-    {"id": 1, "name": "patient 1", "age": 29, "gender": "woman"},
-]  # hier data van patients (age, gender, etc)
-
-# http://localhost:5000/patients?id=0&id=1
-@app.route("/patients", methods=["GET"])
-def get_patients():
-    if "id" in request.args:
-        id = request.args.getlist("id")
-
-    else:
-        return "Error: No id field provided. Please provide patient id"
-
-    results = []
-
-    for patient in patients:
-        for patient_id in id:
-            if patient["id"] == int(patient_id):
-                results.append(patient)
-
-    # return render_template('template', patients = results)
-    return jsonify(results)
-
-
-@app.route("/data", methods=["GET"])
+@app.route("/data", methods=["POST"])
 def show_data():
-    if "id" in request.args:
-        id = request.args.getlist("id")
-
-    else:
-        return "Error: No id field provided. Please provide patient id"
-
-    if "MEG" in request.args:
-        MEG = request.args.get("MEG")
-
-    else:
-        return "Error: No MEG field provided. Please provide patient MEG"
-
-    if "EEG" in request.args:
-        EEG = request.args.get("EEG")
-
-    else:
-        return "Error: No EEG field provided. Please provide patient EEg"
-
-    results = []
-
-    patient_id = "{0:03}".format(int(id[0]))
-    parent_dir = os.path.abspath(os.pardir)
-    path_to_patient = parent_dir + f"\\data\\ICA_processed\\sub{patient_id}\\0.fif"
-
-    print(EEG.split(","))
-    # print(ast.literal_eval(EEG))
-    # print(ast.literal_eval(MEG))
-
-    # return render_template('template', patients = results)
+    data = request.json
+    ids = data["subject"]
+    eegs = ",".join(data["eeg"])
+    megs = ",".join(data["meg"])
+    print(ids)
+    print(eegs)
+    print(megs)
 
     script = server_document(
-        url="http://localhost:5006/app", arguments={"eeg": "fp,p2,f4"}
+        url="http://localhost:5006/app", arguments={"id": ids, "EEG": eegs, "MEG": megs}
+        #arguments={"id": 1, "EEG": "EEG001", "MEG": "MEG0111"}
     )
     return render_template(
-        "index.html",
+        "bokeh-wrapper.html",
         script=script,
-        title="Info Viz Example",
-        paragraph_title="Testing123",
     )
 
 
