4f8796336fd1405a1f7832dbc1f1db324b309f6a
aleverdo <alexis.francois.verdoodt@vub.be>
2022-05-05 12:24:23 +0200

    improved speed of bokeh

--- webapp/bokeh/app.py
+++ webapp/bokeh/app.py
@@ -14,12 +14,14 @@ from bokeh.client import push_session
 from bokeh.io import show
 from bokeh.layouts import column, gridplot, row, layout
 from bokeh.models import Button, ColumnDataSource, PanTool, Tabs, Panel, Range1d, CheckboxButtonGroup, CustomJS, \
-    Spinner, Span
+    Spinner, Span, Dropdown, Select
 from bokeh.palettes import RdYlBu3
 from bokeh.plotting import figure, curdoc
 import pandas as pd
 
 from data.parse import *
+
+import streamlit as st
 
 """
 Get arguments that are given to session
@@ -35,15 +37,14 @@ args = curdoc().session_context.request.arguments
 subject_id = args.get("id")[0].decode("UTF-8").split(",")
 EEG_channels = args.get("EEG")[0].decode("UTF-8").split(",")
 MEG_channels = args.get("MEG")[0].decode("UTF-8").split(",")
-logger.info(subject_id)
 
 TOOLS = "xpan, reset"
 
-start = 1000
-step = 3000
-current = 1000
+start = 10000
+step = 10000
+current = 10000
 
-runs_data = [parse_run(int(subject_id[0]), run, EEG_channels, MEG_channels) for run in range(1, 7)]
+runs_data = parse_run(int(subject_id[0]), 1, EEG_channels, MEG_channels)
 logger.info("logging...")
 
 """
@@ -61,6 +62,8 @@ spinner_max = Spinner(low=0.1, high=1.5, step=0.1, value=0.5, width=80)
 checkbox_button_group_average = CheckboxButtonGroup(labels=["Average"])
 checkbox_button_group_average.on_click(print("ok"))
 
+select_runs = Select(options=["run 1", "run 2", "run 3", "run 4", "run 5", "run 6"])
+
 
 def start_bokeh():
     tabs = []
@@ -71,66 +74,66 @@ def start_bokeh():
     The if should later be used to plot only when channels are selected ==> does not work yet with parse
     """
     if 2 > 1:
-        for i in range(1, 7):
-            colors = itertools.cycle(palette)
+        colors = itertools.cycle(palette)
 
-            EEG_temp_source = []
-            MEG_temp_source = []
+        EEG_temp_source = []
+        MEG_temp_source = []
 
-            EEG_data = runs_data[i - 1][0]
-            MEG_data = runs_data[i - 1][1]
-            events = runs_data[i - 1][2]
+        EEG_data = runs_data[0]
+        MEG_data = runs_data[1]
+        events = runs_data[2]
 
-            EEG_p = figure(title="EEG", plot_height=200, plot_width=1000,
+        EEG_p = figure(title="EEG", plot_height=200, plot_width=1000,
                            tools=TOOLS, output_backend="canvas", x_range=(0, 500), toolbar_location="above")
-            MEG_p = figure(title="MEG", plot_height=200, plot_width=1000,
+        MEG_p = figure(title="MEG", plot_height=200, plot_width=1000,
                            tools=TOOLS, output_backend="canvas", x_range=EEG_p.x_range, toolbar_location=None)
 
-            EEG_p.toolbar.logo = None
+        EEG_p.toolbar.logo = None
 
-            EEG_p.y_range = Range1d(-50, 50)
-            MEG_p.y_range = Range1d(-1500, 1500)
+        EEG_p.y_range = Range1d(-50, 50)
+        MEG_p.y_range = Range1d(-1500, 1500)
 
-            vline = Span(location=0, dimension='height', line_color='red', line_width=145, line_alpha=0.2)
-            EEG_p.renderers.extend([vline])
+        vline = Span(location=0, dimension='height', line_color='red', line_width=145, line_alpha=0.2)
+        EEG_p.renderers.extend([vline])
 
-            EEG_p.xaxis.axis_label = 'seconds'
-            EEG_p.yaxis.axis_label = 'µV'
+        EEG_p.xaxis.axis_label = 'seconds'
+        EEG_p.yaxis.axis_label = 'µV'
 
-            MEG_p.xaxis.axis_label = 'seconds'
-            MEG_p.yaxis.axis_label = 'fT'
-            for channel, color in zip(range(len(EEG_data)), colors):
-                source = ColumnDataSource(
-                    dict(
-                        x=np.arange(0, current, 1),
-                        y=EEG_data[channel][:start]))
-                EEG_p.line(x='x', y='y', line_width=1, source=source, line_color=color)
-                EEG_temp_source.append(source)
+        MEG_p.xaxis.axis_label = 'seconds'
+        MEG_p.yaxis.axis_label = 'fT'
+        for channel, color in zip(range(len(EEG_data)), colors):
+            source = ColumnDataSource(
+                dict(
+                    x=np.arange(0, len(EEG_data[channel]), 1),
+                    y=EEG_data[channel]))
+            EEG_p.line(x='x', y='y', line_width=1, source=source, line_color=color)
+            EEG_temp_source.append(source)
 
-            for channel, color in zip(range(len(MEG_data)), colors):
-                source = ColumnDataSource(
-                    dict(
-                        x=np.arange(0, current, 1),
-                        y=MEG_data[channel][:start]))
-                MEG_p.line(x='x', y='y', line_width=1, source=source, line_color=color)
-                MEG_temp_source.append(source)
+        for channel, color in zip(range(len(MEG_data)), colors):
+            source = ColumnDataSource(
+                dict(
+                    x=np.arange(0, len(MEG_data[channel]), 1),
+                    y=MEG_data[channel]))
+            MEG_p.line(x='x', y='y', line_width=1, source=source, line_color=color)
+            MEG_temp_source.append(source)
 
-            x_ticks_loc = [i * 145 for i in range(500)]
+        x_ticks_loc = [i * 145 for i in range(500)]
 
-            tick_labels = [str(i) for i in range(500)]
-            EEG_p.xaxis.ticker = x_ticks_loc
-            EEG_p.xaxis.major_label_overrides = dict(zip(x_ticks_loc, tick_labels))
+        tick_labels = [str(i) for i in range(500)]
+        EEG_p.xaxis.ticker = x_ticks_loc
+        EEG_p.xaxis.major_label_overrides = dict(zip(x_ticks_loc, tick_labels))
 
-            MEG_p.xaxis.ticker = x_ticks_loc
-            MEG_p.xaxis.major_label_overrides = dict(zip(x_ticks_loc, tick_labels))
-            EEG_sources.append(EEG_temp_source)
-            MEG_sources.append(MEG_temp_source)
+        MEG_p.xaxis.ticker = x_ticks_loc
+        MEG_p.xaxis.major_label_overrides = dict(zip(x_ticks_loc, tick_labels))
+        EEG_sources.append(EEG_temp_source)
+        MEG_sources.append(MEG_temp_source)
 
-            tabs.append(Panel(child=column(EEG_p, MEG_p, sizing_mode="stretch_both"), title=f'Run {i}'))
+        tabs.append(Panel(child=column(EEG_p, MEG_p, sizing_mode="stretch_both"), title=f'Run {1}'))
 
     curdoc().add_root(column(
         column(checkbox_button_group, sizing_mode="fixed"),
         row(column(children=[spinner_min, spinner_max]), column(checkbox_button_group_average)),
+        column(select_runs),
         column(children=[Tabs(tabs=tabs)], sizing_mode="stretch_both"), sizing_mode='stretch_width'))
     return EEG_sources, MEG_sources
 
@@ -168,10 +171,37 @@ def update_callback():
     current = new_current
 
     if current >= len(EEG_data[0]) or current >= len(MEG_data[0]):
-        curdoc().remove_periodic_callback(pc_id)
+        # curdoc().remove_periodic_callback(pc_id)
+        print("ok")
 
     a = a + 1
 
 
+def change_run(attr, old, new):
+    st.legacy_caching.caching.clear_cache()
+    run = int(new.split(" ")[1])
+    runs_data = parse_run(int(subject_id[0]), run, EEG_channels, MEG_channels)
 
-pc_id = curdoc().add_periodic_callback(update_callback, 3000)
+    EEG_data = runs_data[0]
+    MEG_data = runs_data[1]
+
+    for idx, EEG_source in enumerate(EEG_sources[0]):
+        x = np.arange(0, len(EEG_data[idx][:]), 1)
+        y = EEG_data[idx][:]
+        new_data = dict()
+        new_data['x'] = x
+        new_data['y'] = y
+        EEG_source.data = new_data
+
+    for idx, MEG_source in enumerate(MEG_sources[0]):
+        x = np.arange(0, len(MEG_data[idx][:]), 1)
+        y = MEG_data[idx][:]
+        new_data = dict()
+        new_data['x'] = x
+        new_data['y'] = y
+        MEG_source.data = new_data
+
+
+select_runs.on_change('value', change_run)
+
+# pc_id = curdoc().add_periodic_callback(update_callback, 3000)
