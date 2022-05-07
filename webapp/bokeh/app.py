fe26cc844bd5a868bd5feb70c868d6ced9258cda
RobinDHVUB <72613839+RobinDHVUB@users.noreply.github.com>
2022-05-07 17:49:58 +0200

    Update app.py

--- webapp/bokeh/app.py
+++ webapp/bokeh/app.py
@@ -96,7 +96,7 @@ def create_avg_plots(run_idx):
         toolbar_sticky=False,
         sizing_mode="stretch_both",
     )
-    EEG_p.x_range = Range1d(0, view_size, bounds=(0, chunk_size))
+    EEG_p.x_range = Range1d(0, view_size)
     EEG_p.xaxis.ticker = [tick for tick in x_ticks.keys()]
     EEG_p.xaxis.major_label_overrides = x_ticks
     EEG_p.xaxis.axis_label = "Time (s)"
