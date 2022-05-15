eedeebec91999cba5c580e9e899143b57ad33189
RobinDHVUB <72613839+RobinDHVUB@users.noreply.github.com>
2022-05-15 17:14:31 +0200

    Update start.cmd

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,2 +1,2 @@
 cd ..
-python -m venv env && .\\env\Scripts\activate && pip install -r requirements.txt && start chrome http://localhost:5006/run && panel serve visualisation/run.py
+python -m venv env & .\\env\Scripts\activate & pip install -r requirements.txt & start chrome http://localhost:5006/run & panel serve visualisation/run.py
